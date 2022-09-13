6daae45c3d317dce2ee172d85f0949533aff9841
Tyler Kropp <tyler@tylerkropp.xyz>
2022-09-12 22:25:29 -0400

    Compute directly to swapchain image

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
-use std::sync::Arc;
-
-use bytemuck::{Pod, Zeroable};
 use vulkano::{
-    buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess},
-    command_buffer::{
-        AutoCommandBufferBuilder, CommandBufferUsage, RenderPassBeginInfo, SubpassContents,
-    },
+    command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage},
     descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet},
     device::{
         physical::{PhysicalDevice, PhysicalDeviceType},
         Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo,
     },
     format::Format,
-    image::{view::ImageView, ImageUsage, ImageViewAbstract, StorageImage},
-    impl_vertex,
-    instance::{Instance, InstanceCreateInfo},
-    pipeline::{
-        graphics::{
-            input_assembly::{InputAssemblyState, PrimitiveTopology},
-            vertex_input::BuffersDefinition,
-            viewport::{Viewport, ViewportState},
-        },
-        ComputePipeline, GraphicsPipeline, Pipeline, PipelineBindPoint,
+    image::{
+        view::{ImageView, ImageViewCreateInfo},
+        ImageUsage, ImageViewAbstract,
     },
-    render_pass::{Framebuffer, FramebufferCreateInfo, Subpass},
-    sampler::{Filter, Sampler, SamplerAddressMode, SamplerCreateInfo, SamplerMipmapMode},
+    instance::{Instance, InstanceCreateInfo},
+    pipeline::{ComputePipeline, Pipeline, PipelineBindPoint},
     swapchain::{
-        acquire_next_image, AcquireError, Swapchain, SwapchainCreateInfo, SwapchainCreationError,
+        acquire_next_image, AcquireError, SurfaceInfo, Swapchain, SwapchainCreateInfo,
+        SwapchainCreationError,
     },
     sync::{self, FlushError, GpuFuture},
 };
@@ -97,24 +85,27 @@ fn main() {
             enabled_extensions: device_extensions,
             queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
 
-            ..Default::default()
+            ..DeviceCreateInfo::default()
         },
     )
     .unwrap();
 
     let queue = queues.next().unwrap();
 
+    let image_format = Some(
+        physical_device
+            .surface_formats(&surface, SurfaceInfo::default())
+            .unwrap()
+            .into_iter()
+            .find(|(format, _)| *format == Format::R8G8B8A8_UNORM)
+            .unwrap()
+            .0,
+    );
+
     let (mut swapchain, mut images) = {
         let surface_capabilities = physical_device
-            .surface_capabilities(&surface, Default::default())
+            .surface_capabilities(&surface, SurfaceInfo::default())
             .unwrap();
-
-        let image_format = Some(
-            physical_device
-                .surface_formats(&surface, Default::default())
-                .unwrap()[0]
-                .0,
-        );
 
         let (swapchain, images) = Swapchain::new(
             device.clone(),
@@ -123,55 +114,36 @@ fn main() {
                 min_image_count: surface_capabilities.min_image_count,
                 image_format,
                 image_extent: surface.window().inner_size().into(),
-                image_usage: ImageUsage::color_attachment(),
+                image_usage: ImageUsage {
+                    storage: true,
+                    ..ImageUsage::color_attachment()
+                },
                 composite_alpha: surface_capabilities
                     .supported_composite_alpha
                     .iter()
                     .next()
                     .unwrap(),
-                ..Default::default()
+                ..SwapchainCreateInfo::default()
             },
         )
         .unwrap();
         let images = images
             .into_iter()
-            .map(|image| ImageView::new_default(image.clone()).unwrap())
+            .map(|image| {
+                ImageView::new(
+                    image.clone(),
+                    ImageViewCreateInfo {
+                        format: image_format,
+                        ..ImageViewCreateInfo::from_image(&image.clone())
+                    },
+                )
+                .unwrap()
+            })
             .collect::<Vec<_>>();
         (swapchain, images)
     };
 
-    let render_pass = vulkano::single_pass_renderpass!(queue.device().clone(),
-        attachments: {
-            color: {
-                load: Clear,
-                store: Store,
-                format: swapchain.image_format(),
-                samples: 1,
-            }
-        },
-        pass: {
-                color: [color],
-                depth_stencil: {}
-        }
-    )
-    .unwrap();
-    let subpass = Subpass::from(render_pass.clone(), 0).unwrap();
-
     let size = images[0].dimensions().width_height();
-
-    let storage_image: Arc<ImageView<StorageImage>> = StorageImage::general_purpose_image_view(
-        queue.clone(),
-        size,
-        Format::R8G8B8A8_UNORM,
-        ImageUsage {
-            sampled: true,
-            transfer_dst: true,
-            storage: true,
-            color_attachment: true,
-            ..ImageUsage::none()
-        },
-    )
-    .unwrap();
 
     mod cs {
         vulkano_shaders::shader! {
@@ -202,70 +174,6 @@ fn main() {
     )
     .unwrap();
 
-    mod vs {
-        vulkano_shaders::shader! {
-            ty: "vertex",
-            src: "
-                    #version 450
-    
-                    layout(location=0) in vec2 position;
-                    layout(location=1) in vec2 tex_coords;
-                    layout(location = 0) out vec2 f_tex_coords;
-    
-                    void main() {
-                        gl_Position =  vec4(position, 0.0, 1.0);
-                        f_tex_coords = tex_coords;
-                    }
-                "
-        }
-    }
-
-    mod fs {
-        vulkano_shaders::shader! {
-            ty: "fragment",
-            src: "
-                    #version 450
-    
-                    layout(location = 0) in vec2 v_tex_coords;
-                    layout(location = 0) out vec4 f_color;
-                    layout(set = 0, binding = 0) uniform sampler2D tex;
-    
-                    void main() {
-                        f_color = texture(tex, v_tex_coords);
-                    }
-                "
-        }
-    }
-
-    let (vertices, indices) = textured_quad(2.0, 2.0);
-    let vertex_buffer = CpuAccessibleBuffer::<[TexturedVertex]>::from_iter(
-        queue.device().clone(),
-        BufferUsage::vertex_buffer(),
-        false,
-        vertices.into_iter(),
-    )
-    .unwrap();
-    let index_buffer = CpuAccessibleBuffer::<[u32]>::from_iter(
-        queue.device().clone(),
-        BufferUsage::index_buffer(),
-        false,
-        indices.into_iter(),
-    )
-    .unwrap();
-
-    let vs = vs::load(device.clone()).unwrap();
-    let fs = fs::load(device.clone()).unwrap();
-
-    let graphics_pipeline = GraphicsPipeline::start()
-        .vertex_input_state(BuffersDefinition::new().vertex::<TexturedVertex>())
-        .input_assembly_state(InputAssemblyState::new().topology(PrimitiveTopology::TriangleList))
-        .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
-        .vertex_shader(vs.entry_point("main").unwrap(), ())
-        .fragment_shader(fs.entry_point("main").unwrap(), ())
-        .render_pass(subpass.clone())
-        .build(device.clone())
-        .unwrap();
-
     let mut recreate_swapchain = false;
     let mut previous_frame_end = Some(sync::now(device.clone()).boxed());
 
@@ -291,6 +199,7 @@ fn main() {
             if recreate_swapchain {
                 let (new_swapchain, new_images) = match swapchain.recreate(SwapchainCreateInfo {
                     image_extent: dimensions.into(),
+                    image_format,
                     ..swapchain.create_info()
                 }) {
                     Ok(r) => r,
@@ -299,7 +208,16 @@ fn main() {
                 };
                 images = new_images
                     .into_iter()
-                    .map(|image| ImageView::new_default(image.clone()).unwrap())
+                    .map(|image| {
+                        ImageView::new(
+                            image.clone(),
+                            ImageViewCreateInfo {
+                                format: image_format,
+                                ..ImageViewCreateInfo::from_image(&image.clone())
+                            },
+                        )
+                        .unwrap()
+                    })
                     .collect::<Vec<_>>();
                 swapchain = new_swapchain;
                 recreate_swapchain = false;
@@ -334,39 +252,10 @@ fn main() {
             let desc_layout = pipeline_layout.set_layouts().get(0).unwrap();
             let compute_set = PersistentDescriptorSet::new(
                 desc_layout.clone(),
-                [WriteDescriptorSet::image_view(0, storage_image.clone())],
-            )
-            .unwrap();
-            let layout = graphics_pipeline.layout().set_layouts().get(0).unwrap();
-            let sampler = Sampler::new(
-                queue.device().clone(),
-                SamplerCreateInfo {
-                    mag_filter: Filter::Nearest,
-                    min_filter: Filter::Nearest,
-                    address_mode: [SamplerAddressMode::Repeat; 3],
-                    mipmap_mode: SamplerMipmapMode::Nearest,
-                    ..Default::default()
-                },
+                [WriteDescriptorSet::image_view(0, images[image_num].clone())],
             )
             .unwrap();
 
-            let graphics_set = PersistentDescriptorSet::new(
-                layout.clone(),
-                [WriteDescriptorSet::image_view_sampler(
-                    0,
-                    storage_image.clone(),
-                    sampler,
-                )],
-            )
-            .unwrap();
-            let fb = Framebuffer::new(
-                render_pass.clone(),
-                FramebufferCreateInfo {
-                    attachments: vec![images[image_num].clone()],
-                    ..Default::default()
-                },
-            )
-            .unwrap();
             builder
                 .bind_pipeline_compute(compute_pipeline.clone())
                 .bind_descriptor_sets(
@@ -376,35 +265,6 @@ fn main() {
                     compute_set,
                 )
                 .dispatch([size[0], size[1], 1])
-                .unwrap()
-                .set_viewport(
-                    0,
-                    [Viewport {
-                        origin: [0.0, 0.0],
-                        dimensions: [size[0] as f32, size[1] as f32],
-                        depth_range: 0.0..1.0,
-                    }],
-                )
-                .begin_render_pass(
-                    RenderPassBeginInfo {
-                        clear_values: vec![Some([0., 0., 0., 1.].into())],
-                        ..RenderPassBeginInfo::framebuffer(fb)
-                    },
-                    SubpassContents::Inline,
-                )
-                .unwrap()
-                .bind_pipeline_graphics(graphics_pipeline.clone())
-                .bind_descriptor_sets(
-                    PipelineBindPoint::Graphics,
-                    graphics_pipeline.layout().clone(),
-                    0,
-                    graphics_set,
-                )
-                .bind_vertex_buffers(0, vertex_buffer.clone())
-                .bind_index_buffer(index_buffer.clone())
-                .draw_indexed(index_buffer.len() as u32, 1, 0, 0, 0)
-                .unwrap()
-                .end_render_pass()
                 .unwrap();
 
             let command_buffer = builder.build().unwrap();
@@ -431,36 +291,4 @@ fn main() {
         }
         _ => (),
     });
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, Debug, Default, Zeroable, Pod)]
-pub struct TexturedVertex {
-    pub position: [f32; 2],
-    pub tex_coords: [f32; 2],
-}
-impl_vertex!(TexturedVertex, position, tex_coords);
-
-pub fn textured_quad(width: f32, height: f32) -> (Vec<TexturedVertex>, Vec<u32>) {
-    (
-        vec![
-            TexturedVertex {
-                position: [-(width / 2.0), -(height / 2.0)],
-                tex_coords: [0.0, 1.0],
-            },
-            TexturedVertex {
-                position: [-(width / 2.0), height / 2.0],
-                tex_coords: [0.0, 0.0],
-            },
-            TexturedVertex {
-                position: [width / 2.0, height / 2.0],
-                tex_coords: [1.0, 0.0],
-            },
-            TexturedVertex {
-                position: [width / 2.0, -(height / 2.0)],
-                tex_coords: [1.0, 1.0],
-            },
-        ],
-        vec![0, 2, 1, 0, 3, 2],
-    )
 }
